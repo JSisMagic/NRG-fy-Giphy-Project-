e383a0e3af75e6e2e65d3c8422b5b5aad52c112b
Nia Kostadinova <n_deyanova@abv.bg>
2023-07-16 14:10:35 +0300

    Add trending on one line

--- template/src/views/home-view.js
+++ template/src/views/home-view.js
@@ -1,10 +1,10 @@
 import { toMovieSimple } from './movie-views.js';
 
-export const toHomeView = (gifsObj) => `
-<div id="home">
-  <h1>Trending</h1>
-  <div class="content">
-  ${gifsObj.map(toMovieSimple).join('\n')}
+export const toHomeView = (gifsObj, numberOfGifsPerLine = 50) => `
+  <div id="home">
+    <h1>Trending</h1>
+    <div class="content" style="display: flex; flex-wrap: nowrap; overflow-x: auto;">
+      ${gifsObj.slice(0, numberOfGifsPerLine).map(toMovieSimple).join('\n')}
+    </div>
   </div>
-  </div>
-  `;
+`;
