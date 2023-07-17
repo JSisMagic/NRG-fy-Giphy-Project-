3cbfd309b169b3c07e21babe7fa2a77fcb987072
Nia Kostadinova <n_deyanova@abv.bg>
2023-07-17 22:10:13 +0300

    fix gif height, scroll css

--- template/src/components/views/simple-view.js
+++ template/src/components/views/simple-view.js
@@ -2,11 +2,7 @@ import { FULL_HEART, EMPTY_HEART } from '../constants.js';
 
 export const simpleView = (gif) => `
   <div class="gif-simple">
-  <img class="gif-image" src=${gif.images.fixed_width.url} alt=${
-  gif.title
-} width=${+gif.images.fixed_width.width + 90} height=${
-  +gif.images.fixed_width.height + 90
-}>
+  <img class="image-display" src="${gif.images.fixed_height.url}" alt="${gif.title}" data-gif="${gif.id}">
   <div class="details-link"><p class="view-details" data-gif-id="${
     gif.id
   }">View Details</p></div>
