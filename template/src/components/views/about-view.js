30b3d521c812bde4c2192707e934fa1021b65366
Radoslav Frenski <r.frenski@gmail.com>
2023-07-20 14:19:01 +0300

    about view pics

--- template/src/components/views/about-view.js
+++ template/src/components/views/about-view.js
@@ -4,15 +4,15 @@ export const toAboutView = () => `
     <h1>About the app</h1>
     <div class="about-row">
       <div class="about-item">
-        <img class="about-image" src="../images/NiaGiphy.gif" alt="Nia Kostadinova" />
+      <img class="image-display" src="https://media2.giphy.com/media/5KsmdnRDjUCmJzHrSs/200.gif?cid=c320317ckgwaig8h0bz9hc2uhqbj6w90v5bvmx6gbrltsm6d&amp;ep=v1_gifs&amp;rid=200.gif&amp;ct=g" alt="" data-gif="5KsmdnRDjUCmJzHrSs">
         <p class="about-name">Nia Kostadinova</p>
       </div>
       <div class="about-item">
-        <img class="about-image" src="../images/RadoGiphy.gif" alt="Radoslav Frenski" />
+      <img class="image-display" src="https://media4.giphy.com/media/YuK1dnekPzehCMrMVQ/200.gif?cid=c320317csjvxkh21kcthib6a2x9s0ousslqdf64b0o8gfj3d&amp;ep=v1_gifs&amp;rid=200.gif&amp;ct=g" alt="" data-gif="YuK1dnekPzehCMrMVQ">
         <p class="about-name">Radoslav Frenski</p>
       </div>
       <div class="about-item">
-        <img class="about-image" src="../images/GerganaGiphy.gif" alt="Gergana Dragoeva Quievy" />
+      <img class="image-display" src="https://media0.giphy.com/media/2iMFb9WhJ3co3DuroU/200.gif?cid=c320317cjc2bahfzr5wqx4cg9mgmup02bxw6pponw5pfap1b&amp;ep=v1_gifs&amp;rid=200.gif&amp;ct=g" alt="" data-gif="2iMFb9WhJ3co3DuroU">
         <p class="about-name">Gergana Dragoeva Quievy</p>
       </div>
     </div>
