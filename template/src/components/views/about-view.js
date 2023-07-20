b5a16d7f1cafd15dbc6ecec55df0777759576ea2
gerganadq <gerganaquievy@gmail.com>
2023-07-20 13:30:59 +0300

    Add Nia, Rado & Gergana gifs in about-view.js

--- template/src/components/views/about-view.js
+++ template/src/components/views/about-view.js
@@ -2,7 +2,20 @@ export const toAboutView = () => `
 <div id="about">
   <div class="content">
     <h1>About the app</h1>
-    <h2>Authors:<br> Gergana Dragoeva Quievy<br> Radoslav Frenski<br> Nia Kostadinova</h2>
+    <div class="about-row">
+      <div class="about-item">
+        <img class="about-image" src="../images/NiaGiphy.gif" alt="Nia Kostadinova" />
+        <p class="about-name">Nia Kostadinova</p>
+      </div>
+      <div class="about-item">
+        <img class="about-image" src="../images/RadoGiphy.gif" alt="Radoslav Frenski" />
+        <p class="about-name">Radoslav Frenski</p>
+      </div>
+      <div class="about-item">
+        <img class="about-image" src="../images/GerganaGiphy.gif" alt="Gergana Dragoeva Quievy" />
+        <p class="about-name">Gergana Dragoeva Quievy</p>
+      </div>
+    </div>
     <h2>Date: 2023</h2>
   </div>
 </div>
